1f6cdd4ab7e8a91e663115e1ea51561207b407a7
Daniel <dcampoverde@ifinetwork.com>
2023-08-08 11:02:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [views, setViews] = useState("")
+
+    useEffect(()=>{
+      axios.get("https://counterapi-ywst.onrender.com/")
+      .then(res=>{
+        setViews(res.data.counter)
+      })
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p id="Content">{views} views</p>
+    </>
   );
 }
 
